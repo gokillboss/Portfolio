e2d10ae9c3b868ebd46c66003d4bbcd746a610f1
tyla <gokillboss@yahoo.com>
2024-03-31 15:22:16 -0700

    addded 2 more projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,30 +5,50 @@ import ProjectCard from "./ProjectCard";
 import WhaleTrade from "./WhaleTrade.png";
 import Wordle from "./Wordle.png";
 import Nutrition from "./nutrition.png";
+import Portfolio from "./Portfolio.png";
+import BookTrade from './BookTrade.png'
 
 const projects = [
     {
         img: WhaleTrade,
-        langs: ["html", "css", "javascript", "react", "Express", "Node.js, MongoDB"],
+        langs: ["html", "CSS", "javascript", "React", "Express", "Node.js, MongoDB"],
         title: "Whale Trade",
         disc: "Paper trading web app",
         link : 'https://github.com/gokillboss/WhaleTrade'
     },
     {
-        img: Wordle,
-        langs: ["MUI", "html", "css", "javascript", "Node.js", "React", "Hook"],
-        title: "Wordle",
-        disc: "A Word guessing game",
-        link: 'https://github.com/gokillboss/Wordle'
+        img: BookTrade,
+        langs: ["Bootstrap", "CSS", "javascript", "React", "MySQL", "Node.JS"],
+        title: "Portfolio",
+        disc: "Web app using to sell books",
+        link : 'https://github.com/gokillboss/Portfolio'
+
     },
+   
     {
         img: Nutrition,
-        langs: ["MUI", "css", "javascript", "Node.js", "Koa.JS", "MySQL", "React"],
+        langs: ["MUI", "CSS", "javascript", "Node.js", "Koa.JS", "MySQL", "React"],
         title: "Food Trace",
         disc: "Tracking User Nutrition Web App",
         link : 'https://github.com/gokillboss/FoodTrace'
 
     },
+    {
+        img: Portfolio,
+        langs: ["Bootstrap", "CSS", "javascript", "React, AOE"],
+        title: "Portfolio",
+        disc: "You are using it right now",
+        link : 'https://github.com/gokillboss/Portfolio'
+
+    },
+    {
+        img: Wordle,
+        langs: ["MUI", "html", "CSS", "javascript", "Node.js", "React"],
+        title: "Wordle",
+        disc: "A Word guessing game",
+        link: 'https://github.com/gokillboss/Wordle'
+    },
+   
 ];
 
 function Projects() {
@@ -52,7 +72,7 @@ function Projects() {
             </div>
 
             <div data-aos="fade-up" className="mx-auto my-5 pt-5 justify-content-center align-items-center" >
-                <Row xs={1} sm={2} md={3} lg={3} xl={3} className="g-4">
+                <Row xs={1} sm={2} md={2} lg={3} xl={3} className="g-4 ">
                     {projects.map(({ img, langs, title, disc, link }, index) => (
                         <Col xs={12} lg={6}  key={index}>
                             <ProjectCard img={img} langs={langs} title={title} disc={disc} link={link} />
